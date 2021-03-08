078404fd8dd37105f8a824aa96a300842d31b5ce
DeyanDanailov <stormbringer_ss@abv.bg>
2021-03-08 16:05:08 +0200

    entity relations exercise

---
+++ EntityRelations/P01_StudentSystem/Data/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        [Column(TypeName = "varchar(max)")]
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsEnrolled { get; set; } = new HashSet<StudentCourse>();
+        public virtual ICollection<Resource> Resources { get; set; } = new HashSet<Resource>();
+        public virtual ICollection<Homework> HomeworkSubmissions { get; set; } = new HashSet<Homework>();
+    }
+}
